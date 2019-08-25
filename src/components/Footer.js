0f7a7fa10c16aaebf96fc2018f06a125b5684f50
Jamie Magee <jamie.magee@gmail.com>
2019-08-25 21:10:42 +0200

    Playing with layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,43 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Footer = class extends React.Component {
-  render() {
-    return (
-      <footer className="footer has-background-black has-text-white-ter">
-        {/* <div className="content has-text-centered">
-          <img src={logo} alt="Kaldi" style={{ width: '14em', height: '10em' }} />
-        </div> */}
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a className="navbar-item" href="/admin/" target="_blank" rel="noopener noreferrer">
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    )
-  }
-}
+import Footer from 'react-bulma-components/lib/components/footer'
+import Content from 'react-bulma-components/lib/components/content'
+import Container from 'react-bulma-components/lib/components/container'
 
-export default Footer
+export default () => {
+  return (
+    <Footer>
+      <Container>
+        <Content className="has-text-centered">Made by Jamie with ❤️ for Kat</Content>
+      </Container>
+    </Footer>
+  )
+}
