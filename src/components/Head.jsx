521681054bea16b8bdff7ad92191b86623de838b
Jamie Magee <jamie.magee@gmail.com>
2019-09-21 16:08:22 +0200

    Update apple-touch-icon

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -14,7 +14,6 @@ export default () => {
       <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
       <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
 
-      <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
       <meta name="theme-color" content="#fff" />
 
       <meta property="og:type" content="website" />
