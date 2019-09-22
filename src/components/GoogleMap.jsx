0f9a49244b8c5f8a41b64cc1b4fd1be754be42f9
Jamie Magee <jamie.magee@gmail.com>
2019-09-22 14:09:22 +0200

    Fix GPS coords

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -17,12 +17,12 @@ export class MapContainer extends Component {
       >
         <Marker
           name={'Københavns Rådhus'}
-          position={{ lat: 55.67594, lng: 12.56553 }}
+          position={{ lat: 55.675257, lng: 12.5698222 }}
           onClick={() => window.open('https://goo.gl/maps/xEpcfLJT1BzvEqgR8', '_blank')}
         />
         <Marker
           name={'Apollo Kantine'}
-          position={{ lat: 55.68333, lng: 12.58333 }}
+          position={{ lat: 55.680077, lng: 12.588407 }}
           onClick={() => window.open('https://goo.gl/maps/XLHobw14qD44w9Zb9', '_blank')}
         />
       </Map>
